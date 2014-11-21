815324acc37d709439ef1a54d8f92f7539824004
Akawit Panutai <info.akawit@gmail.com>
2014-11-22 01:24:50 +0700

    export sql script into "src/project.sql"
    Added New file
    - ManageGroup.php
    - addCarBrand.php

---
+++ src/project.sql
@@ -0,0 +1,206 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 21, 2014 at 05:45 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `car`
+--
+
+CREATE TABLE IF NOT EXISTS `car` (
+`id` int(11) NOT NULL,
+  `Mapping_CarBrand_CarModel_id` int(11) NOT NULL,
+  `car_year` varchar(4) DEFAULT NULL,
+  `body_number` varchar(45) DEFAULT NULL,
+  `cylinder` varchar(4) DEFAULT NULL,
+  `fuel_tank` varchar(2) DEFAULT NULL,
+  `color` varchar(255) DEFAULT NULL,
+  `detail` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carbrand`
+--
+
+CREATE TABLE IF NOT EXISTS `carbrand` (
+`id` int(10) unsigned NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carimages`
+--
+
+CREATE TABLE IF NOT EXISTS `carimages` (
+`id` int(10) unsigned NOT NULL,
+  `Car_id` int(10) unsigned NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `detail` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carmodel`
+--
+
+CREATE TABLE IF NOT EXISTS `carmodel` (
+`id` int(10) unsigned NOT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mapping_carbrand_carmodel`
+--
+
+CREATE TABLE IF NOT EXISTS `mapping_carbrand_carmodel` (
+`id` int(10) unsigned NOT NULL,
+  `CarBrand_id` int(10) unsigned NOT NULL,
+  `CarModel_id` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE IF NOT EXISTS `member` (
+`id` int(10) unsigned NOT NULL,
+  `Role_id` int(10) unsigned NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `gender` bit(1) DEFAULT NULL,
+  `birthdate` date DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `phoneNumber` varchar(10) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `detail` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE IF NOT EXISTS `role` (
+`id` int(10) unsigned NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `detail` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `car`
+--
+ALTER TABLE `car`
+ ADD PRIMARY KEY (`id`), ADD KEY `CarDetail_FKIndex1` (`Mapping_CarBrand_CarModel_id`);
+
+--
+-- Indexes for table `carbrand`
+--
+ALTER TABLE `carbrand`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `carimages`
+--
+ALTER TABLE `carimages`
+ ADD PRIMARY KEY (`id`), ADD KEY `CarImages_FKIndex1` (`Car_id`);
+
+--
+-- Indexes for table `carmodel`
+--
+ALTER TABLE `carmodel`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mapping_carbrand_carmodel`
+--
+ALTER TABLE `mapping_carbrand_carmodel`
+ ADD PRIMARY KEY (`id`), ADD KEY `Mapping_CarBrand_CarModel_FKIndex1` (`CarModel_id`), ADD KEY `Mapping_CarBrand_CarModel_FKIndex2` (`CarBrand_id`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+ ADD PRIMARY KEY (`id`), ADD KEY `Member_FKIndex1` (`Role_id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `car`
+--
+ALTER TABLE `car`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `carbrand`
+--
+ALTER TABLE `carbrand`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `carimages`
+--
+ALTER TABLE `carimages`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `carmodel`
+--
+ALTER TABLE `carmodel`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `mapping_carbrand_carmodel`
+--
+ALTER TABLE `mapping_carbrand_carmodel`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
