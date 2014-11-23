3c0dbc80d716e565d655c7acf3e7b8caee4946b0
COLORCODE SOFTWARE <colorcode.software@gmail.com>
2014-11-24 00:15:16 +0700

    24/11/2014 : กวาง     - เพิ่มไฟล์ css 1 ไฟล์     - ปรับหน้า UI ใหม่นิดหน่อย

--- Car_showroom/addCarBrand.php
+++ Car_showroom/addCarBrand.php
@@ -29,7 +29,8 @@ if (@$_GET ['submit'] == 'true') {
 
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <link href="css/style.css" rel="stylesheet">
-
+<link href="css/modern-business.css" rel="stylesheet">
+		
 <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
 <!--[if lt IE 9]>
           <script src="js/html5shiv.js"></script>
@@ -52,59 +53,111 @@ if (@$_GET ['submit'] == 'true') {
 </head>
 
 <body>
-	<div class="container">
-		<div class="row clearfix page-header">
-			<div class="col-md-12 column">
-				<div class="row clearfix">
-					<div class="col-md-4 column">
-						<img class="img-rounded" src="http://placehold.it/650x250" alt="">
+	<!-- Navigation panel -->
+	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="index.html">หน้าหลัก</a>
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li>
+						<a href="#">เปรียบเทียบรถยนต์</a>
+					</li>
+					<li>
+						<a href="ManageCarProfile.php">จัดการรถยนต์</a>
+					</li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">จัดการหมวดหมู่รถยนต์<b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li>
+								<a href="ManageBrand.php">จัดการยี่ห้อ</a>
+							</li>
+							<li>
+								<a href="ManageModel.php">จัดการรุ่น</a>
+							</li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">จัดการสมาชิก</a>
+					</li>
+				</ul>
+				<!-- Login form -->
+				<form class="form-inline pull-right" style="margin-top:10px;" role="form">
+					<div class="form-group">
+						<div class="input-group">
+							<label class="sr-only" for="exampleInputEmail2">ชื่อผู้ใช้</label>
+							<input type="email" class="form-control" id="exampleInputEmail2" placeholder="ชื่อผู้ใช้">
+						</div>
 					</div>
-					<div class="col-md-4 column"></div>
+					<div class="form-group">
+						<label class="sr-only" for="exampleInputPassword2">รหัสผ่าน</label>
+						<input type="password" class="form-control" id="exampleInputPassword2" placeholder="รหัสผ่าน">
+					</div>
+					<button type="submit" class="btn btn-default">เข้าสู่ระบบ</button>
+					<div class="form-inline">
+						<label>
+							<a href="#">สมัครสมาชิก</a>
+						</label>
+					</div>
+				</form>
+			</div>
+			<!-- /.navbar-collapse -->
+		</div>
+		<!-- /.container -->
+		<div>
+		</div>
+	</nav>
+	
+	<!-- Header image -->
+	<header id="myCarousel" class="carousel slide">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			<li data-target="#myCarousel" data-slide-to="2"></li>
+		</ol>
 
-					<div class="col-md-8 ">
-						<form class="form-inline pull-right" role="form">
-							<div class="form-group">
-								<div class="input-group">
-									<label class="sr-only" for="exampleInputEmail2">ชื่อผู้ใช้</label>
-									<input type="email" class="form-control"
-										id="exampleInputEmail2" placeholder="ชื่อผู้ใช้">
-								</div>
-							</div>
-							<div class="form-group">
-								<label class="sr-only" for="exampleInputPassword2">รหัสผ่าน</label>
-								<input type="password" class="form-control"
-									id="exampleInputPassword2" placeholder="รหัสผ่าน">
-							</div>
-							<button type="submit" class="btn btn-default">เข้าสู่ระบบ</button>
-							<div class="form-inline">
-								<label> <a href="#">สมัครสมาชิก</a>
-								</label>
-							</div>
-						</form>
-					</div>
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner">
+			<div class="item active">
+				<div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
+				<div class="carousel-caption">
+					<h2>Caption 1</h2>
 				</div>
-				<nav class="navbar navbar-default" role="navigation">
-					<div class="navbar-header pull-right">
-						<button type="button" class="navbar-toggle" data-toggle="collapse"
-							data-target="#bs-example-navbar-collapse-1">
-							<span class="sr-only">หน้าหลัก</span><span class="icon-bar"></span><span
-								class="icon-bar"></span><span class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand" href="#">หน้าหลัก</a>
-						<button type="button" class="navbar-toggle" data-toggle="collapse"
-							data-target="#bs-example-navbar-collapse-1">
-							<span class="sr-only">เปรียบเทียบรถยนต์</span><span
-								class="icon-bar"></span><span class="icon-bar"></span><span
-								class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand" href="#">เปรียบเทียบรถยนต์</a>
-					</div>
-				</nav>
+			</div>
+			<div class="item">
+				<div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
+				<div class="carousel-caption">
+					<h2>Caption 2</h2>
+				</div>
+			</div>
+			<div class="item">
+				<div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
+				<div class="carousel-caption">
+					<h2>Caption 3</h2>
+				</div>
 			</div>
 		</div>
 
-
-
+		<!-- Controls -->
+		<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+			<span class="icon-prev"></span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" data-slide="next">
+			<span class="icon-next"></span>
+		</a>
+	</header>
+	
+	<div class="container">
 		<div class="row clearfix">
 			<div class="col-md-12 column">
 				<div class=" panel panel-default">
