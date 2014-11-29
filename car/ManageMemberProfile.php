e4fec554ab66b9392fd3a44c7f2f755881c71b5e
punkroom <panutai.akawat@gmail.com>
2014-11-29 18:16:07 +0700

    add ManageMemberProfile.php add addMemberProfile.php add editMemberPermission.php add editMemberProfile.php add datepicker.css

---
+++ car/ManageMemberProfile.php
@@ -0,0 +1,107 @@
+﻿<?php
+include 'src/class/ManageCar.php';
+
+$objCar = new ManageCar();
+
+$pageSize = 10;
+$currentPage = 1;
+if (isset($_GET['page'])) {
+    $currentPage = $_GET['page'];
+}
+if (@$_GET['delete'] == 'true') {
+    $id = $_GET['id'];
+    if ($objcar->delete($id))
+        echo '<meta http-equiv=REFRESH CONTENT=0;url=ManageCarProfile.php>';
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>ระบบจัดแสดงรถยนต์</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <?php include 'includes.php' ?>
+    </head>
+
+    <body>
+        <!-- Header -->
+        <?php include 'header.php'; ?>
+
+        <div class="container">
+            <div class="row clearfix">
+                <div class="col-md-12 column">
+                    <div class=" panel panel-default">
+                        <div class="panel-heading">
+                            <h4>รายการสมาชิก</h4>
+                            <small></small>
+                        </div>
+
+                        <div class="panel-body">
+                            <div class="row clearfix">
+                                <div class="col-md-12 column">
+                                    <a href="addCarProfile.php"><button type="button"
+                                                                        class="btn btn-primary btn-lg">เพิ่มสมาชิก</button></a>
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th>ชื่อ</th>
+                                                <th>นามสกุล</th>
+                                                <th>เพศ</th>
+                                                <th>วันเกิด</th>
+                                                <th>ทึ่อยู่</th>
+                                                <th>เบอร์โทรศัพท์</th>
+                                                <th>อีเมลล์</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            $arrCar = $objCar->getCarAllPaging($pageSize, $currentPage);
+                                            foreach ($arrCar as $row) {
+                                                ?>
+                                                <tr>
+                                                    <td>
+                                                        <a href="editCarProfile.php?id=<?php echo $row->id; ?>"<button type="button" class="btn btn-warning btn-lg">แก้ไข&nbsp;</button></a>
+                                                        <a href="ManageCarProfile.php?delete=true&id=<?php echo $row->id; ?>"><button type="button" class="btn btn-danger btn-lg">&nbsp;&nbsp;&nbsp;ลบ&nbsp;&nbsp;</button></a>
+                                                    </td>
+                                                    <td><?php echo $row->brand_name ?></td>
+                                                    <td><?php echo $row->model_name ?></td>
+                                                    <td><?php echo $row->car_year ?></td>
+                                                    <td><?php echo $row->body_number ?></td>
+                                                    <td><?php echo $row->cylinder ?></td>
+                                                    <td><?php echo $row->fuel_tank ?></td>
+                                                </tr>
+                                            <?php } ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+
+                            <nav>
+                                <ul class="pagination">
+                                    <li><a>หน้าที่</a></li>
+                                    <?php $pages = $objCar->getPageing(); ?>
+                                    <li><a href="ManageCarProfile.php?page=<?= ($currentPage - 1) < 1 ? 1 : ($currentPage - 1) ?>"><span aria-hidden="true">&laquo;</span><span class="sr-only">Previous</span></a></li>
+                                    <?php
+                                    for ($i = 1; $i <= $pages; $i++) {
+                                        ?>
+                                        <li><a href="ManageCarProfile.php?page=<?= $i ?>"><?= $i ?></a></li>
+                                        <?php
+                                    }
+                                    ?>
+                                    <li><a href="ManageCarProfile.php?page=<?= ($currentPage + 1) > $pages ? $currentPage : ($currentPage + 1) ?>"><span aria-hidden="true">&raquo;</span><span class="sr-only">Next</span></a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row clearfix">
+            <div class="col-md-12 column"></div>
+        </div>
+        <!-- Footer -->
+        <?php include 'footer.php'; ?>
+    </body>
+</html>
---
+++ car/editMemberProfile.php
@@ -0,0 +1,184 @@
+﻿<?php
+include 'src/class/ManageBrandCar.php';
+include 'src/class/ManageModelCar.php';
+include 'src/class/ManageCar.php';
+
+$objCar = new ManageCar ();
+
+
+if (@$_GET ['submit'] == 'true') {
+    $brandid = $_POST ['brandid'];
+    $modelid = $_POST ['modelid'];
+    $caryear = $_POST ['caryear'];
+    $bodynumber = $_POST ['bodynumber'];
+    $cylinder = $_POST ['cylinder'];
+    $fueltank = $_POST ['fueltank'];
+	$files = $_FILES['files'];
+
+
+
+    if ($objCar->add($brandid, $modelid, $caryear, $bodynumber, $cylinder, $fueltank)){
+		$lastRow = $objCar->getLastCar();
+	
+		for($i=0;$i<10;$i++){
+			if($files['name'][$i]==''){
+				continue;
+			}
+				$folder = 'img/upload/';
+				$name = $files['name'][$i];
+				$ext = explode('.',$name);
+				$newname = $ext[0].'_'.date('YmdHis').'.'.$ext[1];
+				$path = $folder.$newname;
+
+				move_uploaded_file($files['tmp_name'][$i],$path);
+				
+				$objCar->addImage($lastRow->id,$name,$path);
+		}
+		echo '<meta http-equiv=REFRESH CONTENT=0;url=ManageCarProfile.php>';
+	}
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>ระบบจัดการแสดงรถยนต์</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <?php include 'includes.php' ?>
+    </head>
+
+    <body>
+        <!-- Header -->
+        <?php include 'header.php'; ?>
+
+        <div class="container">
+            <div class="row clearfix">
+                <div class="col-md-12 column">
+                    <div class=" panel panel-default">
+                        <div class="panel-heading">
+                            <h4>เพิ่มรถยนต์</h4>
+                            <small></small>
+                        </div>
+
+                        <div class="panel-body">
+						
+                            <div class="row clearfix">
+                                <div class="col-md-12 column">
+
+                                    <br></br>
+                                    <div class="row clearfix">
+									
+									
+                                        <div class="col-md-12 column">
+										
+                                            <div class="panel panel-default">
+
+                                                <div class="panel-body">
+
+                                                    <form class="form-horizontal" role="form"
+                                                          action="addCarProfile.php?submit=true" method="post"  enctype="multipart/form-data">
+										
+										
+														  
+													<hr/>
+                                                        <div class="form-group">
+                                                            <label for="inputEmail3" class="col-sm-2 control-label">ชื่อ</label>
+                                                            <div class="col-md-4">
+                                                                 <input class="form-control" id="inputPassword3"
+                                                                       type="text" name="fueltank" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="inputEmail3" class="col-sm-2 control-label">นามสกุล</label>
+
+                                                            <div class="col-md-4">
+                                                                <input class="form-control" id="inputPassword3"
+                                                                       type="text" name="fueltank" />
+
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="inputEmail3" class="col-sm-2 control-label">เพศ</label>
+                                                            <div class="col-md-4">
+
+                                                                <select class="form-control" name="typecar">
+                                                                    <option value="0">ชาย</option>
+                                                                    <option value="1">หญิง</option>
+
+                                                                </select>
+                                                            </div>
+
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="inputEmail3" class="col-sm-2 control-label">วันเกิด</label>
+                                                            <div class="col-sm-10">
+                                                                <input class="form-control" id="dateData" type="datetime" name="caryear" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="inputPassword3" class="col-sm-2 control-label">ที่อยู่</label>
+                                                            <div class="col-sm-10">
+                                                                <input class="form-control" id="inputPassword3"
+                                                                       type="text" name="bodynumber" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="inputPassword3" class="col-sm-2 control-label">เบอร์โทรศัพท์</label>
+                                                            <div class="col-sm-10">
+                                                                <input class="form-control" id="inputPassword3"
+                                                                       type="text" name="cylinder" />
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="inputPassword3" class="col-sm-2 control-label">อีเมลล์</label>
+                                                            <div class="col-sm-10">
+                                                                <input class="form-control" id="inputPassword3"
+                                                                       type="text" name="fueltank" />
+                                                            </div>
+                                                        </div>
+														
+                                                        <button type="button" class="btn btn-success btn-lg">&nbsp;&nbsp;ล้างข้อมูล&nbsp;&nbsp;</button>
+                                                        <button type="submit" class="btn btn-primary btn-lg">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;แก้ไข&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
+
+														
+														
+                                                    </form>
+                                                </div>
+
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Footer -->
+                    <?php include 'footer.php'; ?>
+
+                    <script type="text/javascript">
+                        $(function() {
+                            $('#brand').change(function() {
+                                var brandid = $('#brand').val();
+                                $('#model').css('visibility', 'visible');
+                                $.post('json/listModel.php', {id: brandid}, function(data) {
+                                    $('#model').html(data);
+                                });
+
+
+                            });
+							
+							$( "#datepicker" ).datepicker();
+							});
+
+                        });
+	
+                    </script>
+
+
+                    </body>
+                    </html>
