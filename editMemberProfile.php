67dba176d5e1eb1bdb14e48a7535ecbd09d9e8eb
COLORCODE SOFTWARE <colorcode.software@gmail.com>
2014-12-19 15:20:55 +0700

    Update editMemberProfile.php
    เอาปุ่ม 'ล้างข้อมูล' ออก

--- editMemberProfile.php
+++ editMemberProfile.php
@@ -103,7 +103,7 @@ include_once 'src/class/MemberSystem.php';
                                                             </div>
                                                         </div>
 
-                                                        <button type="button" class="btn btn-success btn-lg">&nbsp;&nbsp;ล้างข้อมูล&nbsp;&nbsp;</button>
+                                                        <!-- <button type="button" class="btn btn-success btn-lg">&nbsp;&nbsp;ล้างข้อมูล&nbsp;&nbsp;</button> -->
                                                         <button type="submit" class="btn btn-primary btn-lg">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;แก้ไข&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
 
 
