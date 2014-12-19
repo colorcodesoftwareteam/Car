5b74d1b2da1736eeaebb560105037f38b8c1c4a3
akawit panutai <burnlogic2014@users.noreply.github.com>
2014-12-19 13:43:53 +0700

    Update LoginSystem.php

--- src/class/LoginSystem.php
+++ src/class/LoginSystem.php
@@ -8,21 +8,26 @@ class LoginSystem {
     private $user;
     private $pass;
     private $objDB;
+    private $profile;
 
     function __construct() {
         $this->objDB = new Database();
     }
 
-    function login($data) {
-        $StrQuery = "SELECT * FROM MEMBER WHERE email ='" . $data['email'] . "' and phoneNumber = '" . $data['password'] . "'";
-        $rs = $this->objDB->query($StrQuery);
-        $row = mysql_fetch_object($rs);
-        $_SESSION['memberId'] = $row->id;
-        $_SESSION['Role_id'] = $row->Role_id;
-        $_SESSION['name'] = $row->name;
-        $_SESSION['lastname'] = $row->lastname;
-        if ($this->objDB->hasRows())
-            return true;
+    function setData($data) {
+        $this->user = $data['email'];
+        $this->pass = $data['password'];
+    }
+
+    function login() {
+        if (!$this->checkUsername() || !$this->validate()) {
+            return false;
+        }
+        $_SESSION['memberId'] = $this->profile->id;
+        $_SESSION['Role_id'] = $this->profile->Role_id;
+        $_SESSION['name'] = $this->profile->name;
+        $_SESSION['lastname'] = $this->profile->lastname;
+        return true;
     }
 
     function logout() {
@@ -30,8 +35,19 @@ class LoginSystem {
         return true;
     }
 
+    function checkUsername() {
+        $StrQuery = "SELECT * FROM MEMBER WHERE email ='" . $this->user . "'";
+        $this->objDB->query($StrQuery);
+        if ($this->objDB->hasRows())
+            return true;
+    }
+
     function validate() {
-        
+        $StrQuery = "SELECT * FROM MEMBER WHERE email ='" . $this->user . "' and phoneNumber = '" . $this->pass . "'";
+        $rs = $this->objDB->query($StrQuery);
+        $this->profile = mysql_fetch_object($rs);
+        if ($this->objDB->hasRows())
+            return true;
     }
 
 }
