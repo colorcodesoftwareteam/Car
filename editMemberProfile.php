f02e129edeee6ba008e02f440059a25baf3689bf
COLORCODE SOFTWARE <colorcode.software@gmail.com>
2014-12-20 15:27:45 +0700

    Update editMemberProfile.php
    - ปรับขนาดปุ่มให้เล็กลง

--- editMemberProfile.php
+++ editMemberProfile.php
@@ -109,8 +109,8 @@ include_once 'src/class/MemberSystem.php';
                                                                        type="password" name="password" />
                                                             </div>
                                                         </div>
-                                                        <!-- <button type="button" class="btn btn-success btn-lg">&nbsp;&nbsp;ล้างข้อมูล&nbsp;&nbsp;</button> -->
-                                                        <button type="submit" class="btn btn-primary btn-lg">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;แก้ไข&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
+                                                        <!-- <button type="button" class="btn btn-success">&nbsp;&nbsp;ล้างข้อมูล&nbsp;&nbsp;</button> -->
+                                                        <button type="submit" class="btn btn-success">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;แก้ไข&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</button>
 
 
 
