189253448b8c5ae787c06db300cba20a7902730c
COLORCODE SOFTWARE <colorcode.software@gmail.com>
2014-12-22 17:55:13 +0700

    Update ManageMemberProfile.php

--- ManageMemberProfile.php
+++ ManageMemberProfile.php
@@ -1,7 +1,7 @@
 ﻿<?php
-include 'src/class/ManageCar.php';
+include 'src/class/MemberSystem.php';
 
-$objCar = new ManageCar();
+$objMember = new MemberSystem();
 
 $pageSize = 10;
 $currentPage = 1;
@@ -11,7 +11,7 @@ if (isset($_GET['page'])) {
 if (@$_GET['delete'] == 'true') {
     $id = $_GET['id'];
     if ($objcar->delete($id))
-        echo '<meta http-equiv=REFRESH CONTENT=0;url=ManageCarProfile.php>';
+        echo '<meta http-equiv=REFRESH CONTENT=0;url=ManageMemberProfile.php>';
 }
 ?>
 <!DOCTYPE html>
@@ -41,10 +41,11 @@ if (@$_GET['delete'] == 'true') {
                         <div class="panel-body">
                             <div class="row clearfix">
                                 <div class="col-md-12 column">
-                                    <a href="addCarProfile.php"><button type="button" class="btn btn-primary">เพิ่มสมาชิก</button></a>
+                                    <a href="addMemberProfile.php"><button type="button" class="btn btn-primary">เพิ่มสมาชิก</button></a>
                                     <table class="table">
                                         <thead>
                                             <tr>
+                                                <th></th>
                                                 <th>ชื่อ</th>
                                                 <th>นามสกุล</th>
                                                 <th>เพศ</th>
@@ -56,20 +57,21 @@ if (@$_GET['delete'] == 'true') {
                                         </thead>
                                         <tbody>
                                             <?php
-                                            $arrCar = $objCar->getCarAllPaging($pageSize, $currentPage);
-                                            foreach ($arrCar as $row) {
+                                            $arrMember = $objMember->getMemberAllPaging($pageSize, $currentPage);
+                                            foreach ($arrMember as $row) {
                                                 ?>
                                                 <tr>
                                                     <td>
-                                                        <a href="editCarProfile.php?id=<?php echo $row->id; ?>"<button type="button" class="btn btn-warning">แก้ไข&nbsp;</button></a>
-                                                        <a href="ManageCarProfile.php?delete=true&id=<?php echo $row->id; ?>"><button type="button" class="btn btn-danger">&nbsp;&nbsp;&nbsp;ลบ&nbsp;&nbsp;</button></a>
+                                                        <a href="editMemberProfile.php?id=<?php echo $row->id; ?>"<button type="button" class="btn btn-warning">แก้ไข&nbsp;</button></a>
+                                                        <a href="ManageMemberProfile.php?delete=true&id=<?php echo $row->id; ?>"><button type="button" class="btn btn-danger">&nbsp;&nbsp;&nbsp;ลบ&nbsp;&nbsp;</button></a>
                                                     </td>
-                                                    <td><?php echo $row->brand_name ?></td>
-                                                    <td><?php echo $row->model_name ?></td>
-                                                    <td><?php echo $row->car_year ?></td>
-                                                    <td><?php echo $row->body_number ?></td>
-                                                    <td><?php echo $row->cylinder ?></td>
-                                                    <td><?php echo $row->fuel_tank ?></td>
+                                                    <td><?php echo $row->name ?></td>
+                                                    <td><?php echo $row->lastname ?></td>
+                                                    <td><?php echo $row->gender ?></td>
+                                                    <td><?php echo $row->birthdate ?></td>
+                                                    <td><?php echo $row->address ?></td>
+                                                    <td><?php echo $row->phoneNumber ?></td>
+                                                    <td><?php echo $row->email ?></td>
                                                 </tr>
                                             <?php } ?>
                                         </tbody>
@@ -78,19 +80,7 @@ if (@$_GET['delete'] == 'true') {
                             </div>
 
                             <nav>
-                                <ul class="pagination">
-                                    <li><a>หน้าที่</a></li>
-                                    <?php $pages = $objCar->getPageing(); ?>
-                                    <li><a href="ManageCarProfile.php?page=<?= ($currentPage - 1) < 1 ? 1 : ($currentPage - 1) ?>"><span aria-hidden="true">&laquo;</span><span class="sr-only">Previous</span></a></li>
-                                    <?php
-                                    for ($i = 1; $i <= $pages; $i++) {
-                                        ?>
-                                        <li><a href="ManageCarProfile.php?page=<?= $i ?>"><?= $i ?></a></li>
-                                        <?php
-                                    }
-                                    ?>
-                                    <li><a href="ManageCarProfile.php?page=<?= ($currentPage + 1) > $pages ? $currentPage : ($currentPage + 1) ?>"><span aria-hidden="true">&raquo;</span><span class="sr-only">Next</span></a></li>
-                                </ul>
+                                <?php $objMember->getPageing($currentPage); ?>
                             </nav>
                         </div>
                     </div>
