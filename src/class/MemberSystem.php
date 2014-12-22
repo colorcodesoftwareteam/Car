d29a87b5103b9e18b9c23c5b6fb6c0ecb726cfc8
COLORCODE SOFTWARE <colorcode.software@gmail.com>
2014-12-22 17:53:24 +0700

    Update MemberSystem.php
    - เพิ่ม function : getMemberAllPaging, getPageing

--- src/class/MemberSystem.php
+++ src/class/MemberSystem.php
@@ -5,7 +5,9 @@ include_once 'Database.php';
 class MemberSystem {
 
     private $objDB;
-
+    private $NUM_ROWS;
+    private $PAGE_SIZE;
+    
     function __construct() {
         $this->objDB = new Database();
     }
@@ -36,5 +38,40 @@ class MemberSystem {
         }
         return $arrauIterator;
     }
+    
+    function getMemberAllPaging($pageSize, $page) {
+        $this->PAGE_SIZE = $pageSize;
+        $lim_start = (($page - 1) * $pageSize);
+        $lim_end = $pageSize;
+        $limit = "limit " . $lim_start . ", " . $lim_end;
 
+        $str = "select m.id, m.role_id, m.name, m.lastname, m.gender, m.birthdate, m.address, m.phoneNumber, 
+                m.email, m.password, m.detail, m.create_dt, m.update_dt, r.name as role_name, r.detail as role_detail 
+                from member m left join role r on m.role_id=r.id 
+                order by m.create_dt desc ";
+
+        $this->NUM_ROWS = mysql_num_rows($this->objDB->query($str));
+        $rs = $this->objDB->query($str . $limit);
+        $arrayIterator = new ArrayIterator();
+
+        while ($row = mysql_fetch_object($rs)) {
+            $arrayIterator->append($row);
+        }
+        return $arrayIterator;
+    }
+    
+    function getPageing($currentPage) {
+        $pages = ceil($this->NUM_ROWS / $this->PAGE_SIZE);
+        
+        $temp = '<ul class="pagination">';
+        $temp .= '<li><a>หน้าที่</a></li>';
+        $temp .= '<li><a href="ManageMemberProfile.php?page=' .(($currentPage - 1) < 1 ? 1 : ($currentPage - 1)). '"><span aria-hidden="true">&laquo;</span><span class="sr-only">Previous</span></a></li>';
+        for ($i = 1; $i <= $pages; $i++) {
+            $temp .= '<li><a href="ManageMemberProfile.php?page=' .$i. '">' .$i. '</a></li>';
+        }
+        $temp .= '<li><a href="ManageMemberProfile.php?page=' .(($currentPage + 1) > $pages ? $currentPage : ($currentPage + 1)). '"><span aria-hidden="true">&raquo;</span><span class="sr-only">Next</span></a></li>';
+        $temp .= '</ul>';
+        
+        echo $temp;
+    }
 }
