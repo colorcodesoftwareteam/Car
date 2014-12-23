0f8532d8f6ecb69facca1023a08ae13ee12df01d
COLORCODE SOFTWARE <colorcode.software@gmail.com>
2014-12-23 21:39:14 +0700

    23/12/2014 : Kwang     - ลบไฟล์ที่ไม่ได้ใช้งาน

--- includes.php
+++ includes.php
@@ -16,14 +16,6 @@
         <![endif]-->
 
 <!-- Fav and touch icons -->
-<link rel="apple-touch-icon-precomposed" sizes="144x144"
-      href="img/apple-touch-icon-144-precomposed.png">
-<link rel="apple-touch-icon-precomposed" sizes="114x114"
-      href="img/apple-touch-icon-114-precomposed.png">
-<link rel="apple-touch-icon-precomposed" sizes="72x72"
-      href="img/apple-touch-icon-72-precomposed.png">
-<link rel="apple-touch-icon-precomposed"
-      href="img/apple-touch-icon-57-precomposed.png">
 <link rel="shortcut icon" href="img/favicon.png">
 
 <script type="text/javascript" src="js/jquery.min.js"></script>
